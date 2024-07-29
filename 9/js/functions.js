6275fb68258558ff5230f41f683c7e6a1048982b
Keks <keks@htmlacademy.ru>
2024-07-29 16:43:33 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/functions.js
@@ -0,0 +1,37 @@
+function checkStringLength(string , maxLength) {
+  const length = string.length;
+  return length <= maxLength;
+}
+
+checkStringLength();
+
+function checkPalindrome(string) {
+  const normalizedString = string.replaceAll().toLowerCase();
+  let reversedString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+
+  return reversedString === normalizedString;
+}
+
+checkPalindrome();
+
+function checkMeeting(startDay, endkDay, meetingStart, meetingDuration) {
+  function timeToMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const workStart = timeToMinutes(startDay);
+  const workEnd = timeToMinutes(endkDay);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStart && meetingEndMinutes <= workEnd;
+}
+
+
+checkMeeting();
